620d9c620d2fcf9f2a671ca1a99f3a314dad5fcb
JW <114599894+judewashock518@users.noreply.github.com>
2023-01-28 11:59:15 -0500

    created 'table', 'thead' and 'tbody' and added headers + data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from 'react';
+
 function App() {
+
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}])
+
   return (
     <div>
-      <h3>Hello World</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>175</td>
+            <td>11-23-2021</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
